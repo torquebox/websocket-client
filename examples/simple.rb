724ebb8988491ac9a07e7aa080cbe38b6302e919
Bob McWhirter <bob@mcwhirter.org>
2011-06-09 20:44:29 -0400

    Initial import.

---
+++ examples/simple.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+$: << File.dirname(__FILE__) + '/lib'
+
+require 'web_socket_client'
+
+#WebSocketClient.connect( 'ws://localhost:8081/websockets/' ) do |client|
+#end
+
+WebSocketClient.new( 'ws://localhost:8081/websockets/' ) do |client|
+  client.on_message do |msg|
+    puts "received #{msg}"
+  end
+
+  client.connect() do |client|
+    puts "sending"
+    client.send( "HOWDY-1" )
+    client.send( "HOWDY-2" )
+    client.send( "HOWDY-3" )
+    sleep(1)
+  end
+end
